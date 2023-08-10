dd36079f054d8fabc645ff2f446c152fdceb71fb
watulojoe <johannezwatulo@gmail.com>
2023-08-11 00:18:04 +0300

    local settings

--- loginApp/settings.py
+++ loginApp/settings.py
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("looks like no local file. you must be live")
+
