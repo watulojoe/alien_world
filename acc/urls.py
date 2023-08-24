bd2ac7de13115daefbfb840390580f7ce285f7cb
watulojoe <johannezwatulo@gmail.com>
2023-08-24 20:38:56 +0300

    tmplates1

--- acc/urls.py
+++ acc/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name="home"),
+    path("signup", views.signup_user, name="signup"),
+    path("login", views.login_user, name="login"),
+    path("logout", views.logout_user, name="logout"),
+
+
 ]
--- loginApp/settings.py
+++ loginApp/settings.py
@@ -15,6 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# settings for sending email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER  = 'johannezwatulo@gmail.com'
+EMAIL_HOST_PASSWORD = 'slaouvqamzsunzkj'
+EMAIL_PORT = 587
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
