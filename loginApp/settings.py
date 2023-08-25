a884382a3bd15d87b1e6362d839bf42291ce991c
watulojoe <johannezwatulo@gmail.com>
2023-08-25 10:27:19 +0300

    stsdf

--- loginApp/settings.py
+++ loginApp/settings.py
@@ -19,15 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER  = 'johannezwatulo@gmail.com'
-EMAIL_HOST_PASSWORD = 'slaouvqamzsunzkj'
+EMAIL_HOST_USER  = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h@!vclrkl_#(ub^-+$n+$$pl&f8woh+m=oqy@bvs)#6ctwj&cb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
